b3b6bcdba968fb39f846691616a23e1078f4ea6f
Tarciso Loiola <tarcisoloiola@gmail.com>
2020-02-28 23:39:44 -0500

    ommit

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(routes)
 //development
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Artipro'
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/Artipro'
 //production
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://artihausapi:artihaus4153@ds149998.mlab.com:49998/heroku_09kfqkw9'
+const MONGODB_URI = process.env.MONGODB_URI
 mongoose.Promise = Promise
 mongoose.connect(MONGODB_URI,
   {
